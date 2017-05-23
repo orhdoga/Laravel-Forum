8502d54b367c185219c4b6be03913b8b09db7b14
= <=>
2017-05-23 20:52:25 +0200

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User;
+        $admin->name = 'Admin';
+        $admin->email = 'info@forum.com';
+        $admin->password = bcrypt('admin');
+        $admin->save();
+    }
+}
